122a6cd571a554a3598de8f75d47f4feff0594ac
kypanz <kypanz95@gmail.com>
2022-04-08 15:50:36 -0300

    idea, you can change the components

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import Appbar from './components/Appbar';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 
-// ES6 style
-import { meterify } from "meterify";
-const Web3 = require("web3"); // Recommend using require() instead of import here
-
-const web3 = meterify(new Web3(), "http://localhost:8669");
-
-web3.eth.getBlock("latest").then(res => console.log(res));
-
 
 function App() {
   return (
